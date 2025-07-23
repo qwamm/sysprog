1c641cbadf0b9a1481493de7fb1f5f7b3779325b
afonin.is <fdfdas@DESKTOP-AVR5EGL.localdomain>
2025-07-24 02:07:51 +0300

    execution of commands without pipeline implemented

--- 2/solution.c
+++ 2/solution.c
@@ -10,6 +10,7 @@
 #include <sys/types.h>
 #include <errno.h>
 #include <signal.h>
+#include <limits.h>
 #include <fcntl.h>
 
 static void shut_down_error_messages()
@@ -22,16 +23,37 @@ static void shut_down_error_messages()
 	}
 }
 
+static void execute_exit(const struct command *cmd)
+{
+	int exit_code = 0;
+	if (cmd->arg_count > 0)
+		exit_code = atoi(cmd->args[0]);
+	exit(exit_code);
+}
+
+static int execute_cd(const struct command *cmd)
+{
+	const char *path = ".";
+        if (cmd->arg_count > 0)
+		path = cmd->args[0];
+
+        if (chdir(path) == 0) {
+                        return 0;
+        }
+        return 1;
+}
+
 static bool command_existence(const char *cmd)
 {
 	if (strchr(cmd, '/') != NULL) return access(cmd, X_OK) == 0;
 
 	const char *cmd_path = getenv("PATH");
-	if (PATH == NULL) return false;
+	if (cmd_path == NULL) return false;
 
 	bool access_to_path = false;
-	char *path_copy = strdup(path);
+	char *path_copy = strdup(cmd_path);
 	char *dir = strtok(path_copy, ":");
+
 
 	while (dir != NULL)
 	{
@@ -40,7 +62,7 @@ static bool command_existence(const char *cmd)
 
 		if (access(full_path, X_OK) == 0)
 		{
-			found = true;
+			access_to_path = true;
 			break;
 		}
 
@@ -52,13 +74,74 @@ static bool command_existence(const char *cmd)
 	return access_to_path;
 }
 
-static int execute_command(struct command *cmd)
+static bool is_pipeline(const struct command_line *line)
 {
-	if (!command_existence(cmd->exe)) return 1;
+	assert(line != NULL);
+
+	struct expr *cur_expr = line->head;
+
+	while (cur_expr != NULL && cur_expr->next != NULL)
+	{
+		if (cur_expr->next->type == EXPR_TYPE_PIPE)
+		{
+			return true;
+		}
+		cur_expr = cur_expr->next;
+	}
+
+	return false;
+}
+
+static int execute_command(const struct command *cmd)
+{
+	if (strcmp(cmd->exe, "exit") == 0)
+	{
+		execute_exit(cmd);
+	}
+	else if (strcmp(cmd->exe, "cd") == 0)
+	{
+		return execute_cd(cmd);
+	}
+	else if (!command_existence(cmd->exe))
+	{
+		return 1;
+	}
+
+	pid_t pid = fork();
+
+	if (pid < 0) return 1;
+
+	if (pid == 0)
+	{
+		shut_down_error_messages();
+
+		//Выделение памяти размера, соответствующего кол-ву аргументов + NULL и имя команды
+		char **args = malloc((cmd->arg_count + 2)*sizeof(char*));
+		if (args == NULL) exit(1);
+
+		args[0] = cmd->exe;
+
+		for (uint32_t i = 1; i < cmd->arg_count + 1; i++)
+		{
+			args[i] = cmd->args[i-1];
+		}
+
+		args[cmd->arg_count + 1] = NULL;
+
+		execvp(cmd->exe, args);
+		free(args);
+		exit(1);
+	}
+	else
+	{
+		int ret_status;
+		waitpid(pid, &ret_status, 0);
+		return WEXITSTATUS(ret_status);
+	}
 
 }
 
-static void execute_command_line(const struct command_line *line)
+static int execute_command_line(const struct command_line *line)
 {
 	assert(line != NULL);
 
@@ -66,27 +149,17 @@ static void execute_command_line(const struct command_line *line)
 
 	if (e->type == EXPR_TYPE_COMMAND && strcmp(e->cmd.exe, "exit") == 0 && e->next == NULL)
 	{
-		int exit_code = 0;
-		if (e->cmd.arg_count > 0)
-			exit_code = atoi(e->cmd.args[0]);
-		exit(exit_code);
+		execute_exit(&e->cmd);
 	}
 	else if (e->type == EXPR_TYPE_COMMAND && strcmp(e->cmd.exe, "cd") == 0 && e->next == NULL &&
 	 line->out_type == OUTPUT_TYPE_STDOUT)
 	{
-		const char *path = ".";
-		if (cmd->arg_count > 0)
-			path = cmd->args[0];
-
-		if (chdir(path) == 0) {
-			return 0;
-		}
-		return 1;
+		return execute_command(&e->cmd);
 	}
-	else if (is_pipe(line))
-	{
-		return execute_piped_command(line);
-	}
+	//else if (is_pipeline(line))
+	//{
+	//	return execute_piped_command(line);
+	//}
 	else if (line->out_type != OUTPUT_TYPE_STDOUT)
 	{
 		/* Создаём новый процесс */
@@ -97,7 +170,7 @@ static void execute_command_line(const struct command_line *line)
 		/* Обработка команды дочерним процессом */
 		if (pid == 0)
 		{
-			shut_down_error_messages()
+			shut_down_error_messages();
 
 			int fd;
 			if (line->out_type == OUTPUT_TYPE_FILE_NEW)
@@ -114,17 +187,21 @@ static void execute_command_line(const struct command_line *line)
 			dup2(fd, STDOUT_FILENO);
 			close(fd);
 
-			return execute_command;
+			int ret_status = execute_command(&e->cmd);
+
+			exit(ret_status);
 		}
 		else
 		{
 			int ret_status;
-			wait(pid, &ret_status, 0);
+			waitpid(pid, &ret_status, 0);
 			return WEXITSTATUS(ret_status);
 		}
 	}
 
+	return execute_command(&e->cmd);
 }
+
 
 int main(void)
 {
